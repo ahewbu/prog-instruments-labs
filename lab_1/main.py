03bbeb513ab36e76d13f006bd663cee11bdf4d2e
ahewbu <shlnsn@outlook.com>
2024-12-18 23:51:11 +0400

    renamed variables, functions, classes

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,83 +3,83 @@ import matplotlib.pyplot as plt
 import scipy
 
 
-M_0 = np.array([0, 1])
-M_1 = np.array([-1, -1])
-M_2 = np.array([2, 1])
+m_0 = np.array([0, 1])
+m_1 = np.array([-1, -1])
+m_2 = np.array([2, 1])
 
-B_0 = np.array([[0.35, 0.15], [0.1, 0.35]])
+b_0 = np.array([[0.35, 0.15], [0.1, 0.35]])
 
-B_1 = np.array([[0.45, 0.15], [0.15, 0.45]])
-B_2 = np.array([[0.15, 0.02], [0.02, 0.15]])
-B_3 = np.array([[0.25, -0.17], [-0.17, 0.25]])
+b_1 = np.array([[0.45, 0.15], [0.15, 0.45]])
+b_2 = np.array([[0.15, 0.02], [0.02, 0.15]])
+b_3 = np.array([[0.25, -0.17], [-0.17, 0.25]])
 
 
-def get_errors_first(sample_1, sample_2, weights, wN):
+def get_errors_first(sample_1, sample_2, weights, w_n):
     counter_0 = 0
     counter_1 = 0
 
-    W = weights[0:2]
+    w = weights[0:2]
 
     for x in sample_1:
-        if W.T @ x + wN > 0:
+        if w.T @ x + w_n > 0:
             counter_0 += 1
 
     for x in sample_2:
-        if W.T @ x + wN < 0:
+        if w.T @ x + w_n < 0:
             counter_1 += 1
 
     print(f"Ошибка первого рода: {counter_0 / sample_1.shape[0]}")
     print(f"Ошибка второго рода: {counter_1 / sample_2.shape[0]}")
 
 
-def get_errors(sample_1, sample_2, W_POPOL):
+def get_errors(sample_1, sample_2, w_popol):
     counter_0 = 0
     counter_1 = 0
 
-    W = W_POPOL[0:2]
-    wN = W_POPOL[2]
+    w = w_popol[0:2]
+    w_n = w_popol[2]
 
     for x in sample_1:
-        if W.T @ x + wN > 0:
+        if w.T @ x + w_n > 0:
             counter_0 += 1
 
     for x in sample_2:
-        if W.T @ x + wN < 0:
+        if w.T @ x + w_n < 0:
             counter_1 += 1
 
     print(f"Ошибка первого рода: {counter_0 / sample_1.shape[0]}")
     print(f"Ошибка второго рода: {counter_1 / sample_2.shape[0]}")
 
 
-def Mahalanobis_distance(M0, M1, B):
-    return (M1 - M0) @ np.linalg.inv(B) @ np.transpose(M1 - M0)
+def mahalanobis_distance(m0, m1, b):
+    return (m1 - m0) @ np.linalg.inv(b) @ np.transpose(m1 - m0)
 
 
-def boundary_of_bayes_classifier_for_N_with_same_B(x, M_l, M_j, B, threshold):
-    M_dif = M_l - M_j
-    M_dif_T = M_dif.reshape(1, 2)
-    M_sum = (M_l + M_j)
-    M_sum_T = M_sum.reshape(1, 2)
-    B_inv = np.linalg.inv(B)
+def boundary_of_bayes_classifier_for_n_with_same_b(x, m_l, m_j, b, threshold):
+    m_dif = m_l - m_j
+    m_dif_t = m_dif.reshape(1, 2)
+    m_sum = (m_l + m_j)
+    m_sum_t = m_sum.reshape(1, 2)
+    b_inv = np.linalg.inv(b)
 
     # d_lj = d_l - d_j = 0
     # d_lj = ... = ax+b=0
     # a_0*x_0 + a_1*x_1 = const
-    a = np.matmul(M_dif_T, B_inv)
-    b = -0.5 * np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 2
+    a = np.matmul(m_dif_t, b_inv)
+    b = -0.5 * np.matmul(np.matmul(m_sum_t, b_inv), m_dif) + threshold  # page 31 случай 2
 
     return np.array((-b - a[0, 0] * x) / a[0, 1])
 
 
-def get_count_fail(x, M_l, M_j, B, threshold):
-    M_dif = M_l - M_j
-    M_dif_T = M_dif.reshape(1, 2)
-    M_sum = (M_l + M_j)
-    M_sum_T = M_sum.reshape(1, 2)
-    B_inv = np.linalg.inv(B)
+def get_count_fail(x, m_l, m_j, b, threshold):
+    m_dif = m_l - m_j
+    m_dif_t = m_dif.reshape(1, 2)
+    m_sum = (m_l + m_j)
+    m_sum_t = m_sum.reshape(1, 2)
+    b_inv = np.linalg.inv(b)
 
-    a = np.matmul(M_dif_T, B_inv)
-    b = -0.5 * np.matmul(np.matmul(M_sum_T, B_inv), M_dif) + threshold  # page 31 случай 2
+    a = np.matmul(m_dif_t, b_inv)
+    b = -0.5 * np.matmul(np.matmul(m_sum_t, b_inv), m_dif) + threshold  # page 31 случай 2
 
     # print(a)
     # print(x)
@@ -95,21 +95,21 @@ def get_count_fail(x, M_l, M_j, B, threshold):
     return counter / x.shape[0]
 
 
-def boundary_of_bayes_classifier_for_N(x, M_l, M_j, B_l, B_j, threshold):
-    M_l_T = M_l.reshape(1, 2)
-    M_j_T = M_j.reshape(1, 2)
+def boundary_of_bayes_classifier_for_n(x, m_l, m_j, b_l, b_j, threshold):
+    m_l_t = m_l.reshape(1, 2)
+    m_j_t = m_j.reshape(1, 2)
 
-    B_l_inv = np.linalg.inv(B_l)
-    B_j_inv = np.linalg.inv(B_j)
+    b_l_inv = np.linalg.inv(b_l)
+    b_j_inv = np.linalg.inv(b_j)
 
-    det_B_l = np.linalg.det(B_l)
-    det_B_j = np.linalg.det(B_j)
+    det_b_l = np.linalg.det(b_l)
+    det_b_j = np.linalg.det(b_j)
 
     # d_lj = d_l - d_j = 0
     # d_lj = ... = xTAx+bx+c=0
-    A = 0.5 * (B_j_inv - B_l_inv)
-    b = np.matmul(M_l_T, B_l_inv) - np.matmul(M_j_T, B_j_inv)
-    c = (0.5 * np.matmul(np.matmul(M_j_T, B_j_inv), M_j) - 0.5 * np.matmul(np.matmul(M_l_T, B_l_inv), M_l) + 0.5 * np.log(det_B_j / det_B_l) + threshold)  # конец 4 лекции
+    a = 0.5 * (b_j_inv - b_l_inv)
+    b = np.matmul(m_l_t, b_l_inv) - np.matmul(m_j_t, b_j_inv)
+    c = (0.5 * np.matmul(np.matmul(m_j_t, b_j_inv), m_j) - 0.5 * np.matmul(np.matmul(m_l_t, b_l_inv), M_l) + 0.5 * np.log(det_b_j / det_b_l) + threshold)  # конец 4 лекции
 
     # print(f"A = {A}")
     # print(f"b = {b}")
@@ -117,24 +117,28 @@ def boundary_of_bayes_classifier_for_N(x, M_l, M_j, B_l, B_j, threshold):
 
     boundary = []
     for x0 in x:
-        coef_a = A[1, 1]
-        coef_b = (A[1, 0] + A[0, 1]) * x0 + b[0, 1]
-        coef_c = A[0, 0] * x0 ** 2 + b[0, 0] * x0 + c[0]
-        D = coef_b ** 2 - 4 * coef_a * coef_c
-        if D > 0:
-            x1_0 = (-coef_b + np.sqrt(D)) / (2 * coef_a)
-            x1_1 = (-coef_b - np.sqrt(D)) / (2 * coef_a)
+        coef_a = a[1, 1]
+        coef_b = (a[1, 0] + a[0, 1]) * x0 + b[0, 1]
+        coef_c = a[0, 0] * x0 ** 2 + b[0, 0] * x0 + c[0]
+        d = coef_b ** 2 - 4 * coef_a * coef_c
+        if d > 0:
+            x1_0 = (-coef_b + np.sqrt(d)) / (2 * coef_a)
+            x1_1 = (-coef_b - np.sqrt(d)) / (2 * coef_a)
             boundary.append([x0, x1_0])
             boundary.append([x0, x1_1])
-        elif D == 0:
+        elif d == 0:
             x1_0 = -coef_b / (2 * coef_a)
             boundary.append([x0, x1_0])
 
     return np.array(boundary)
 
 
-def get_erroneous_classification_probabilities(M_l, M_j, B):
-    distance = Mahalanobis_distance(M_l, M_j, B)
+class mahalanobis_distance:
+    pass
+
+
+def get_erroneous_classification_probabilities(m_l, m_j, b):
+    distance = mahalanobis_distance(m_l, m_j, b)
     print(f"Расстояние Махаланобиса: {distance}")
     p_0 = scipy.stats.norm.cdf(-0.5 * np.sqrt(distance))
     p_1 = 1 - scipy.stats.norm.cdf(0.5 * np.sqrt(distance))
@@ -144,14 +148,14 @@ def get_erroneous_classification_probabilities(M_l, M_j, B):
     print(f"Суммарный вероятность ошибочной классификации: {p_0 + p_1}")
 
 
-def experimental_probability_error(x, M_l, M_j, B_l, B_j):
+def experimental_probability_error(x, m_l, m_j, b_l, b_j):
     count = 0
 
-    calc_d = lambda vec, M, B, P: (np.log(P) - np.log(np.sqrt(np.linalg.det(B))) - 0.5 * np.matmul(np.matmul((vec - M), np.linalg.inv(B)), (vec - M).reshape(2, 1)))  # page 29
+    calc_d = lambda vec, m, b, p: (np.log(p) - np.log(np.sqrt(np.linalg.det(b))) - 0.5 * np.matmul(np.matmul((vec - m), np.linalg.inv(b)), (vec - m).reshape(2, 1)))  # page 29
 
     for xi in x:
-        d_l = calc_d(xi, M_l, B_l, 0.5)
-        d_j = calc_d(xi, M_j, B_j, 0.5)
+        d_l = calc_d(xi, m_l, b_l, 0.5)
+        d_j = calc_d(xi, m_j, b_j, 0.5)
 
         if d_j > d_l:
             count += 1
@@ -159,15 +163,15 @@ def experimental_probability_error(x, M_l, M_j, B_l, B_j):
     return count / x.shape[0]
 
 
-def get_eps(p, N):
-    return np.sqrt((1 - p) / (N * p))
+def get_eps(p, n):
+    return np.sqrt((1 - p) / (n * p))
 
 
-def get_N(p, err):
+def get_n(p, err):
     return (1 - p) / (err ** 2 * p)
 
 
-def task_1():
+def task_1(boundary_of_bayes_classifier_for_n_with_same_b=None):
     # Data with same B
     sample_1 = np.load("Files/arrayX2_1.npy")
     sample_2 = np.load("Files/arrayX2_2.npy")
@@ -179,7 +183,7 @@ def task_1():
 
     x = np.linspace(min_value, max_value, 100)
     threshold = np.log(0.5 / 0.5)
-    y = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, threshold)
+    y = boundary_of_bayes_classifier_for_n_with_same_b(x, m_0, m_1, b_0, threshold)
 
     # plt.suptitle("Байесовский классификатор")
     # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
@@ -187,9 +191,9 @@ def task_1():
     # plt.plot(x, y, color="red")
     # plt.show()
 
-    get_erroneous_classification_probabilities(M_0, M_1, B_0)
+    get_erroneous_classification_probabilities(m_0, m_1, b_0)
 
-    exper = experimental_probability_error(sample_1, M_0, M_1, B_0, B_0, 0.5, 0.5)
+    exper = experimental_probability_error(sample_1, m_0, m_1, b_0, b_0, 0.5, 0.5)
     print(f"Экспериментальная вероятность для БК {exper}")
 
     return x, y
@@ -209,10 +213,10 @@ def task_2():
 
     # MinMax classifier
     # p_0 = p_1 => Laplace functions equal => lambda = 0 => P(omega0) = P(omega1)
-    P_0 = 0.5
-    P_1 = 0.5
-    threshold = np.log(P_0 / P_1)
-    y_0 = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, threshold)
+    p_0 = 0.5
+    p_1 = 0.5
+    threshold = np.log(p_0 / p_1)
+    y_0 = boundary_of_bayes_classifier_for_n_with_same_b(x, m_0, m_1, b_0, threshold)
 
     # plt.suptitle("Минимаксный классификатор")
     # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
@@ -222,11 +226,11 @@ def task_2():
 
     # Neyman-Pearson classifier
     p_0 = 0.05
-    distance = Mahalanobis_distance(M_0, M_1, B_0)
+    distance = mahalanobis_distance(m_0, m_1, b_0)
     # inv Laplace function for (1-p_0): p_0 = 0.05
     inv_laplace = 1.645
     lambda_tilda = -0.5 * distance + np.sqrt(distance) * inv_laplace
-    y_1 = boundary_of_bayes_classifier_for_N_with_same_B(x, M_0, M_1, B_0, lambda_tilda)
+    y_1 = boundary_of_bayes_classifier_for_n_with_same_b(x, m_0, m_1, b_0, lambda_tilda)
 
     # plt.suptitle("Классификатор Неймана-Пирсона")
     # plt.plot(sample_1[:, 0], sample_1[:, 1], color='blue', linestyle='none', marker='.')
@@ -234,7 +238,7 @@ def task_2():
     # plt.plot(x, y_1, color="red")
     # plt.show()
 
-    print(f"Экспериментальная верояность для Неймана-Пирсона: {get_count_fail(sample_1, M_0, M_1, B_0, lambda_tilda)}")
+    print(f"Экспериментальная верояность для Неймана-Пирсона: {get_count_fail(sample_1, m_0, m_1, b_0, lambda_tilda)}")
 
     return [(x, y_0), (x, y_1)]
 
@@ -253,9 +257,9 @@ def task_3():
     # x = np.linspace(-2, 2, 200)
     x = np.linspace(min_value, max_value, 200)
 
-    boundary_01 = boundary_of_bayes_classifier_for_N(x, M_0, M_1, B_1, B_2, 0)
-    boundary_02 = boundary_of_bayes_classifier_for_N(x, M_0, M_2, B_1, B_3, 0)
-    boundary_12 = boundary_of_bayes_classifier_for_N(x, M_1, M_2, B_2, B_3, 0)
+    boundary_01 = boundary_of_bayes_classifier_for_n(x, m_0, m_1, b_1, b_2, 0)
+    boundary_02 = boundary_of_bayes_classifier_for_n(x, m_0, m_2, b_1, b_3, 0)
+    boundary_12 = boundary_of_bayes_classifier_for_n(x, m_1, m_2, b_2, b_3, 0)
 
     plt.ylim(-2, 3)
     plt.xlim(min_value, max_value)
@@ -268,13 +272,13 @@ def task_3():
     plt.scatter(boundary_12[:, 0], boundary_12[:, 1], color="red", s=[5])
     plt.show()
 
-    p = experimental_probability_error(sample_1, M_0, M_1, B_1, B_2, 0.5, 0.5)
+    p = experimental_probability_error(sample_1, m_0, m_1, b_1, b_2, 0.5, 0.5)
     print(f"Экспериментальные вероятности ошибочной классификации: {p}")
 
     eps = get_eps(p, sample_1.shape[0])
     print(f"Относительная погрешность: {eps}")
 
-    n = get_N(p, 0.05)
+    n = get_n(p, 0.05)
     print(f"Объем обучающей выборки, обеспечивающий"
           f"получение оценок вероятностей ошибочной классификации"
           f"с погрешностью не более 5%: {n}")
